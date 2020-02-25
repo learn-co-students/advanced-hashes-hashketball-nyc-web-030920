f2fffc9a61eb83b8f17fa8eb1bfe3a5157c69503
isabelxklee <isabelxklee@gmail.com>
2020-02-25 15:01:57 -0500

    changed hash to be literal

--- hashketball.rb
+++ hashketball.rb
@@ -1,59 +1,39 @@
 def game_hash
-    hash = {}
+    hash = {
+        :home => {
+            :team_name => "Brooklyn Nets",
+            :colors => ["Black", "White"],
+            :players => [
+                {:player_name=>"Alan Anderson", :number=>0, :shoe=>16, :points=>22, :rebounds=>12, :assists=>12, :steals=>3, :blocks=>1, :slam_dunks=>1},
+                {:player_name=>"Reggie Evans", :number=>30, :shoe=>14, :points=>12, :rebounds=>12, :assists=>12, :steals=>12, :blocks=>12, :slam_dunks=>7},
+                {:player_name=>"Brook Lopez", :number=>11, :shoe=>17, :points=>17, :rebounds=>19, :assists=>10, :steals=>3, :blocks=>1, :slam_dunks=>15},
+                {:player_name=>"Mason Plumlee", :number=>1, :shoe=>19, :points=>26, :rebounds=>11, :assists=>6, :steals=>3, :blocks=>8, :slam_dunks=>5},
+                {:player_name=>"Jason Terry", :number=>31, :shoe=>15, :points=>19, :rebounds=>2, :assists=>2, :steals=>4, :blocks=>11, :slam_dunks=>1},
+            ]
+        },
 
-# this is the home team's hash
-
-    hash[:home] = {}
-
-        hash[:home][:team_name] = ""
-        hash[:home][:colors] = []
-        hash[:home][:players] = []
-
-        home_players = hash[:home][:players]
-
-        home_players.push({:player_name => ""})
-        home_players.push({:number => ""})
-        home_players.push({:shoe => ""})
-        home_players.push({:points => ""})
-        home_players.push({:rebounds => ""})
-        home_players.push({:assists => ""})
-        home_players.push({:steals => ""})
-        home_players.push({:blocks => ""})
-        home_players.push({:slam_dunks => ""})
-
-        # Stat	Info	Info	Info	Info	Info
-        # Player Name	Alan Anderson	Reggie Evans	Brook Lopez	Mason Plumlee	Jason Terry
-        # Number	0	30	11	1	31
-        # Shoe	16	14	17	19	15
-        # Points	22	12	17	26	19
-        # Rebounds	12	12	19	11	2
-        # Assists	12	12	10	6	2
-        # Steals	3	12	3	3	4
-        # Blocks	1	12	1	8	11
-        # Slam Dunks	1	7	15	5	1
-
-# this is the away team's hash
-
-    hash[:away] = {}
-
-        hash[:away][:team_name] = ""
-        hash[:away][:colors] = []
-        hash[:away][:players] = []
-
-        away_players = hash[:away][:players]
-
-        away_players.push({:player_name => ""})
-        away_players.push({:number => ""})
-        away_players.push({:shoe => ""})
-        away_players.push({:points => ""})
-        away_players.push({:rebounds => ""})
-        away_players.push({:assists => ""})
-        away_players.push({:steals => ""})
-        away_players.push({:blocks => ""})
-        away_players.push({:slam_dunks => ""})
+        :away => {
+            :team_name => "", 
+            colors => [],
+            :players => []
+        }
+    }
 
     hash 
 end 
 
 puts game_hash
 
+###########################################
+# trying to write this method in a dynamic way
+
+# hash[:home] = {}
+
+# hash[:home][:team_name] = "Brooklyn Nets"
+# hash[:home][:colors] = ["Black", "White"]
+# hash[:home][:players] = []
+
+# home_players = hash[:home][:players]
+
+# home_players.push({:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1})
+# home_players.push({:player_name => "Reggie Evans", :number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7})
